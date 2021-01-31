24bc65cb6a88869a17fd9f6116ec6105562b8c28
Denis Orionov <orionovdv@gmail.com>
2021-01-31 11:18:15 +0300

    Add charts

--- dashboard/views.py
+++ dashboard/views.py
@@ -26,26 +26,33 @@ def main_view(request):
     date_end = datetime.strptime('07.09.2020', '%d.%m.%Y')
     date_list = [date.strftime('%d.%m.%Y') for date in sorted(list(set([flow.date for flow in Flow.objects.filter(date__gte=date_from, date__lte=date_end)])))]
     datasets = []
-    color = ['blue', 'red', 'green', 'yellow']
 
-    for account in Account.objects.filter(currency__name="RUB").filter(currency__company__name="ETP_GPB"):
-        inflow_list = [int(flow.inflow) for flow in
-                       Flow.objects.filter(account=account).filter(date__gte=date_from, date__lte=date_end)]
+    charts_data = {}
 
-        datasets.append(
-            {
-                'label': account.name,
-                'backgroundColor': 'transparent',
-                'borderColor': color.pop(),
-                'data': inflow_list,
-            }
-        )
+    for company in companies:
+        print(company.name)
+        color = ['blue', 'green', 'yellow', 'red']
+        for account in Account.objects.filter(currency__name="RUB").filter(currency__company__name=company.name):
+            print(account)
+            inflow_list = [int(flow.inflow) for flow in
+                    Flow.objects.filter(account=account).filter(date__gte=date_from, date__lte=date_end)]
+            datasets.append(
+                {
+                    'label': account.name,
+                    'backgroundColor': 'transparent',
+                    'borderColor': color.pop(),
+                    'data': inflow_list,
+                }
+            )
 
-    chart_data = {
-        'labels': date_list,
-        'datasets': datasets,
-    }
+        chart_data = {
+            'labels': date_list,
+            'datasets': datasets,
+        }
+
+        charts_data[company.name] = json.dumps(chart_data)
+        datasets = []
 
     return render(request, 'dashboard/index.html',
                   context={'form': date_form, 'companies': companies, 'flows': flows, 'currencies': currencies,
-                           'date': date.strftime('%d.%m.%Y'), 'chart_data': json.dumps(chart_data)})
+                           'date': date.strftime('%d.%m.%Y'), 'charts_data': charts_data})
