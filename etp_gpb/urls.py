d068787b5ae449545d5ccd442c91f7ee88936b82
Denis Orionov <orionovdv@gmail.com>
2021-02-01 14:37:31 +0300

    Add static url settings

--- etp_gpb/urls.py
+++ etp_gpb/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -9,6 +10,5 @@ urlpatterns = [
     path('', main_view)
 ]
 
-urlpatterns += ('',
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+urlpatterns += static(settings.STATIC_URL,
+                      document_root=settings.STATIC_ROOT)
