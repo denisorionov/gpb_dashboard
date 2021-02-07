27e0c19021b2cd59237249d0fc4f780ee1664b85
Denis Orionov <orionovdv@gmail.com>
2021-02-07 20:24:25 +0300

    Update datepicker

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,4 +5,4 @@ from django.forms import SelectDateWidget
 
 
 class DateForm(forms.Form):
-    date = forms.DateField(widget=forms.DateInput(attrs={'class': 'datepicker', 'data-date-format': "mm.dd.yyyy"}))
+    date = forms.DateField(widget=forms.DateInput(attrs={'class': 'form-control pull-right', 'data-date-format': "mm/dd/yyyy", 'id': 'date'}))
